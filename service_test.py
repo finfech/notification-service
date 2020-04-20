5e6c7ce9d16245d327815e036a07765daf6e1a14
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-04-20 10:06:31 +0000

    Fix broken tests

--- service_test.py
+++ service_test.py
@@ -12,15 +12,15 @@ def to_json(obj):
 
 class HandlerUnitTest(TestCase):
     def test_expected_return_error_not_defined_aws_region_env(self):
-        expected = to_json({"msg": "AWS_REGION is not defined on env"})
+        expected = to_json({"msg": "SES_AWS_REGION is not defined on env"})
         actual = handler({}, None)
 
         self.assertEqual(expected, actual)
 
     def test_expected_return_error_not_defined_sender_email_env(self):
-        os.environ['AWS_REGION'] = 'test'
+        os.environ['SES_AWS_REGION'] = 'test'
 
-        expected = to_json({"msg": "SENDER_EMAIL is not defined on env"})
+        expected = to_json({"msg": "SES_SENDER_EMAIL is not defined on env"})
         actual = handler({}, None)
 
         self.assertEqual(expected, actual)
