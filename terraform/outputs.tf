1b9f7e525b9722c221cf55658858d857d7a4960e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-04-25 14:56:27 +0000

    Add notification_queue_url output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "request_notification_queue_arn" {
+output "notification_queue_arn" {
   value = aws_sqs_queue.request_notification_queue.arn
+}
+
+output "notification_queue_url" {
+  value = aws_sqs_queue.request_notification_queue.id
 }
